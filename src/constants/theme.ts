a1bcbc9aae2771c90ee0cd9213f2d898a46a557a
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-01-26 18:37:08 +0100

    💄 Update colors and logo

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -2,8 +2,8 @@ import { ColorScheme } from '../types/theme';
 
 export const THEME: ColorScheme = {
     dark: {
-        primaryColor: '#F4A261',
-        secondaryColor: '#79cce7',
+        primaryColor: '#EE7674',
+        secondaryColor: '#BDD9BF',
         primaryBackgroundColor: '#121212',
         secondaryBackgroundColor: '#000000',
         primaryTextColor: '#E0E0E0',
@@ -12,15 +12,15 @@ export const THEME: ColorScheme = {
         sectionBackgroundColor: '#000',
         sectionTextColor: '#FFF',
         sidebarTextColor: '#000',
-        sidebarActiveColor: 'rgba(244, 162, 97, 0.6)',
-        sidebarHoverColor: 'rgba(121, 204, 231, 0.6)',
+        sidebarActiveColor: 'rgba(238, 118, 116, 0.6)',
+        sidebarHoverColor: 'rgba(189, 217, 191, 0.6)',
         footerBackgroundColor: 'rgb(242, 242, 247)',
         footerTextColor: 'rgb(99, 99, 102)',
         invalidBorderColor: '#ff0000'
     },
     light: {
-        primaryColor: '#F4A261',
-        secondaryColor: '#79cce7',
+        primaryColor: '#EE7674',
+        secondaryColor: '#BDD9BF',
         primaryBackgroundColor: '#fafafc',
         secondaryBackgroundColor: '#FFFFFF',
         primaryTextColor: '#000000',
@@ -29,8 +29,8 @@ export const THEME: ColorScheme = {
         sectionBackgroundColor: '#000',
         sectionTextColor: '#FFF',
         sidebarTextColor: '#000',
-        sidebarActiveColor: 'rgba(244, 162, 97, 0.6)',
-        sidebarHoverColor: 'rgba(121, 204, 231, 0.6)',
+        sidebarActiveColor: 'rgba(238, 118, 116, 0.6)',
+        sidebarHoverColor: 'rgba(189, 217, 191, 0.6)',
         footerBackgroundColor: 'rgb(242, 242, 247)',
         footerTextColor: 'rgb(99, 99, 102)',
         invalidBorderColor: '#ff0000'
