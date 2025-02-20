a5f143a0c10de4388bae294c91d0753c770cb347
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-02-20 13:34:15 +0100

    ✨ Add blinkit

--- src/constants/tools.ts
+++ src/constants/tools.ts
@@ -1,6 +1,8 @@
 import fairFitBanner from '../assets/FairFit_Banner.png';
+import blinkItBanner from '../assets/BlinkIt_Banner.png';
 import { Tool } from '../types/tools';
 
 export const TOOLS: Tool[] = [
-    { name: 'FairFit', url: 'https://fairfit.net', image: fairFitBanner }
+    { name: 'FairFit', url: 'https://fairfit.net', image: fairFitBanner },
+    { name: 'BlinkIt', url: 'https://blink-it-pdf.netlify.app/', image: blinkItBanner }
 ];
