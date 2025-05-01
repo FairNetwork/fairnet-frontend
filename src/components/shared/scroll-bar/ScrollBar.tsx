e7d6819cffc9a630a9d17626a05ff7232a4885e2
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-05-01 17:56:20 +0200

    💄 Update scrollbar

--- src/components/shared/scroll-bar/ScrollBar.tsx
+++ src/components/shared/scroll-bar/ScrollBar.tsx
@@ -9,8 +9,10 @@ type ScrollBarProps = {
 
 const ScrollBar: FC<ScrollBarProps> = ({ children, topOffset = 0, bottomOffset = 0 }) => {
     const containerRef = useRef<HTMLDivElement>(null);
+
     const [thumbHeight, setThumbHeight] = useState(0);
     const [thumbTop, setThumbTop] = useState(0);
+    const [isScrollable, setIsScrollable] = useState(false);
 
     const updateScroll = () => {
         const container = containerRef.current;
@@ -18,6 +20,15 @@ const ScrollBar: FC<ScrollBarProps> = ({ children, topOffset = 0, bottomOffset =
 
         const { scrollTop, scrollHeight, clientHeight } = container;
         const availableHeight = clientHeight - topOffset - bottomOffset;
+
+        const canScroll = scrollHeight > clientHeight;
+        setIsScrollable(canScroll);
+
+        if (!canScroll) {
+            setThumbHeight(0);
+            setThumbTop(0);
+            return;
+        }
 
         const heightRatio = clientHeight / scrollHeight;
         const newThumbHeight = Math.max(heightRatio * availableHeight, 20); // Mindesthöhe
@@ -47,14 +58,16 @@ const ScrollBar: FC<ScrollBarProps> = ({ children, topOffset = 0, bottomOffset =
             <div className="scroll-bar__scroll-container" id="scroll-bar" ref={containerRef}>
                 {children}
             </div>
-            <div
-                className="scroll-bar__scroll-thumb"
-                style={{
-                    height: `${thumbHeight}px`,
-                    top: `${thumbTop}px`,
-                    bottom: `${bottomOffset}px`
-                }}
-            />
+            {isScrollable && (
+                <div
+                    className="scroll-bar__scroll-thumb"
+                    style={{
+                        height: `${thumbHeight}px`,
+                        top: `${thumbTop}px`,
+                        bottom: `${bottomOffset}px`
+                    }}
+                />
+            )}
         </div>
     );
 };
